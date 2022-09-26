464b6e3a46a7833874b01283d1e8b5c2af10c40a
OlViks <salova.40@yandex.ru>
2022-09-26 20:33:02 +0300

    сделал второе задание

--- src/Array.java
+++ src/Array.java
@@ -8,13 +8,42 @@ public class Array {
     }
     public static void talks1_1(){
         int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        for (int i = 0; i < 3; i++) {
+            if (i != 2)
+                System.out.print(array[i] + ", ");
+            else
+                System.out.print(array[i]);
+        }
+        System.out.println();
 
     }
     public static void talks1_2(){
         double[] array = {1.57, 7.654, 9.986 };
+         for (int i = 0; i < 3; i++) {
+            if (i != 2)
+                System.out.print(array[i] + ", ");
+            else
+                System.out.print(array[i]);
+        }
+        System.out.println();
     }
     public static void talks1_3(){
-        int[] array = new int[15];
+        int[] array = new int[4];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        for (int i = 0; i < 4; i++) {
+            if (i != 3)
+                System.out.print(array[i] + ", ");
+            else
+                System.out.println(array[i]);
+        }
+        System.out.println();
+
     }
     public static void talks2_1(){
 
