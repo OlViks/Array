5404512d0fd1b531bdf5d36f4cb18ce8a05e4290
OlViks <salova.40@yandex.ru>
2022-09-26 20:23:27 +0300

    сделал первое задание

---
+++ src/Array.java
@@ -0,0 +1,23 @@
+public class Array {
+    public static void main(String[] args){
+    talks1_1();
+    talks1_2();
+    talks1_3();
+    talks2_1();
+
+    }
+    public static void talks1_1(){
+        int[] array = new int[3];
+
+    }
+    public static void talks1_2(){
+        double[] array = {1.57, 7.654, 9.986 };
+    }
+    public static void talks1_3(){
+        int[] array = new int[15];
+    }
+    public static void talks2_1(){
+
+    }
+
+}
